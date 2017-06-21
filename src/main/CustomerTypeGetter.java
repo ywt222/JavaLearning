dbe2d192b087fecb401c4bd61bb7523a4c2e0665
Wenting Yan <wenting.yan@rea-group.com>
2017-06-21 22:56:20 +0800

    Use try/catch to catch customer type not correct scenario.

--- src/main/CustomerTypeGetter.java
+++ src/main/CustomerTypeGetter.java
@@ -5,6 +5,11 @@ import main.model.CustomerType;
 public class CustomerTypeGetter {
     public static CustomerType getCustomerType(String inputInfo) {
         int start = inputInfo.indexOf(":");
-        return CustomerType.valueOf(inputInfo.substring(0, start));
+        try {
+            return CustomerType.valueOf(inputInfo.substring(0, start));
+        } catch (Exception e) {
+            System.out.println("Customer Type is not correct. Error: " + e);
+            throw e;
+        }
     }
 }
